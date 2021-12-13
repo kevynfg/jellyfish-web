e4e7edf5640c5173afcbf07484fa3f68e7228347
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-12-13 11:43:57 -0300

    fix console log

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,49 +1,65 @@
-import React from 'react'
-import {Form, Formik} from 'formik'
+import React from "react";
+import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
-import { useRegisterMutation } from '../generated/graphql';
+import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/dist/client/router";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-      const router = useRouter();
-      const [, register] = useRegisterMutation();
-        return (
-           <Wrapper variant="small">
-               <Formik initialValues={{email: '', username: '', password: ''}} onSubmit={async (values, {setErrors}) => {
-                  console.log(values)
-                  const response = await register({options: values});
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "", username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register({ options: values });
 
-                  if (response.data?.register.errors) {
-                    setErrors(toErrorMap(response.data.register.errors))
-                  } else if (response.data?.register.user) {
-                    router.push("/");
-                  }
-                }}>
-               {({isSubmitting}) => (
-                  <Form>
-                      <InputField name='username' placeholder='username' label='Username'/>
-                      <Box mt={4}>
-                        <InputField name='email' placeholder='email' label='Email'/>
-                      </Box>
-                      <Box mt={4}>
-                        <InputField name='password' placeholder='password' label='Password' type='password'/>
-                      </Box>
-                    <Button mt={4} type='submit' isLoading={isSubmitting} colorScheme='teal'>Register</Button>
-                  </Form> 
-                )}
-                </Formik> 
-           </Wrapper>
-        );
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
-export default withUrqlClient(createUrqlClient)(Register)
+export default withUrqlClient(createUrqlClient)(Register);
